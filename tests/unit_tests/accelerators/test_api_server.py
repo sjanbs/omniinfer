27c817f02fdca28b53d1a5b84271cc96f9d9d23d
OuyangLan <ouyanglan1@huawei.com>
2025-12-25 18:34:54 +0800

    add api server gpt_oss test; fix bug

--- tests/unit_tests/accelerators/test_api_server.py
+++ tests/unit_tests/accelerators/test_api_server.py
@@ -1,12 +1,115 @@
+import asyncio
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, AsyncMock
 import os
+from pathlib import Path
 import requests
-
-from infer_engines.vllm.vllm.entrypoints.openai import serving_engine 
-from infer_engines.vllm.vllm.engine.multiprocessing.client import MQLLMEngineClient
+from typing import Optional
+from dataclasses import dataclass
 
 from test_proxy import setup_teardown
+
+from vllm.entrypoints.openai import serving_engine 
+from vllm.engine.multiprocessing.client import MQLLMEngineClient
+from vllm.entrypoints.openai.protocol import ChatCompletionRequest
+from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
+from vllm.entrypoints.openai.serving_models import (BaseModelPath,
+                                                    OpenAIServingModels)
+from vllm.transformers_utils.tokenizer import get_tokenizer
+from vllm.outputs import RequestOutput
+
+CUR_DIR = Path(__file__).parent
+MODEL_NAME=f"{CUR_DIR}/mock_model/"
+CHAT_TEMPLATE = "Dummy chat template for testing {}"
+BASE_MODEL_PATHS = [BaseModelPath(name=MODEL_NAME, model_path=MODEL_NAME)]
+
+
+def test_chat_completions_gptoss():
+    @dataclass
+    class MockHFConfig:
+        model_type: str = "gpt_oss"
+
+
+    @dataclass
+    class MockModelConfig:
+        task = "generate"
+        tokenizer = MODEL_NAME
+        trust_remote_code = False
+        tokenizer_mode = "auto"
+        max_model_len = 100
+        tokenizer_revision = None
+        truncation_side = 'left'
+        hf_config = MockHFConfig()
+        logits_processor_pattern = None
+        diff_sampling_param: Optional[dict] = None
+        allowed_local_media_path: str = ""
+        encoder_config = None
+        generation_config: str = "auto"
+
+        def get_diff_sampling_param(self):
+            return self.diff_sampling_param or {}
+
+    with patch("vllm.entrypoints.harmony_utils.get_encoding"),\
+        patch("vllm.entrypoints.openai.serving_chat.parse_chat_output"),\
+        patch("vllm.entrypoints.openai.serving_chat.get_streamable_parser_for_assistant"):
+
+        mock_engine = MagicMock(spec=MQLLMEngineClient)
+        mock_engine.get_tokenizer.return_value = get_tokenizer(MODEL_NAME)
+        mock_output = MagicMock()
+        mock_output_outputs_with_index = MagicMock()
+        mock_output_outputs_with_index.index = 0
+        mock_output_outputs_with_index.finish_reason = "stop"
+        mock_output.outputs = [mock_output_outputs_with_index]
+        mock_generator = AsyncMock()
+        mock_generator.__aiter__.return_value = iter([mock_output, mock_output])
+        mock_engine.generate.return_value = mock_generator
+        mock_engine.errored = False
+
+        models = OpenAIServingModels(engine_client=mock_engine,
+                                    base_model_paths=BASE_MODEL_PATHS,
+                                    model_config=MockModelConfig())
+        serving_chat = OpenAIServingChat(mock_engine,
+                                        MockModelConfig(),
+                                        models,
+                                        response_role="assistant",
+                                        chat_template=CHAT_TEMPLATE,
+                                        chat_template_content_format="auto",
+                                        request_logger=None)
+        req = ChatCompletionRequest(
+            model=MODEL_NAME,
+            messages=[{
+                "role": "user",
+                "content": "what is 1+1?"
+            }],
+        )
+
+        try:
+            asyncio.run(serving_chat.create_chat_completion(req))
+
+        except Exception as e:
+            pytest.fail(f"OpenAIServing.__init__ raised unexpected exception: {e}")
+
+        # Reset generator
+        mock_generator.__aiter__.return_value = iter([mock_output, mock_output])
+        req = ChatCompletionRequest(
+            model=MODEL_NAME,
+            messages=[{
+                "role": "user",
+                "content": "what is 1+1?"
+            }],
+            stream=True,
+        )
+
+        try:
+            async def test_async_output():
+                response = await serving_chat.create_chat_completion(req)
+                async for r in response:
+                    pass
+
+            asyncio.run(test_async_output())
+
+        except Exception as e:
+            pytest.fail(f"OpenAIServing.__init__ raised unexpected exception: {e}")
 
 
 def test_tokenizer_worker_num_cannot_exceed_available_cores():
@@ -71,7 +174,7 @@ def test_api_server_health(setup_teardown):
         if not path_exists:
             os.remove(file_path)
 
-def test_completion_using_process_pool(setup_teardown):
+def test_completions_using_process_pool(setup_teardown):
     from test_proxy import prefill_port_list
 
     url = f"http://127.0.0.1:{prefill_port_list[0]}/v1/completions"
@@ -98,7 +201,7 @@ def test_completion_using_process_pool(setup_teardown):
     print("Response Body (preview):", response.text[:200] + "..." if len(response.text) > 200 else response.text)
     assert response.status_code == 200
 
-def test_chat_completion_using_proc_pool(setup_teardown):
+def test_chat_completions_using_proc_pool(setup_teardown):
     from test_proxy import prefill_port_list
 
     url = f"http://127.0.0.1:{prefill_port_list[0]}/v1/chat/completions"
--- tests/unit_tests/accelerators/test_chunked_prefill_scheduler.py
+++ tests/unit_tests/accelerators/test_chunked_prefill_scheduler.py
@@ -2,11 +2,148 @@ import os
 from pathlib import Path
 from unittest.mock import patch
 import sys
+import torch
+from typing import Optional
+from unittest.mock import Mock
+
+from vllm.config import (CacheConfig, KVTransferConfig, ModelConfig,
+                         SchedulerConfig, SpeculativeConfig, VllmConfig)
+from vllm.multimodal.inputs import MultiModalKwargs, PlaceholderRange
+from vllm.sampling_params import SamplingParams
+from vllm.v1.core.sched.scheduler import Scheduler
+from vllm.v1.kv_cache_interface import (FullAttentionSpec, KVCacheConfig,
+                                        KVCacheGroupSpec)
+from vllm.v1.request import Request
+from vllm.v1.structured_output import StructuredOutputManager
+
+EOS_TOKEN_ID = 50256
+
+
+def create_scheduler(
+    model: str = "facebook/opt-125m",
+    max_num_seqs: int = 16,
+    max_num_batched_tokens: int = 8192,
+    enable_prefix_caching: Optional[bool] = None,
+    long_prefill_token_threshold: int = 0,
+    disable_chunked_mm_input: bool = False,
+    use_kv_connector: bool = False,
+    num_blocks: int = 10000,
+    block_size: int = 16,
+    max_model_len: Optional[int] = None,
+    num_speculative_tokens: Optional[int] = None,
+) -> Scheduler:
+    '''Create scheduler under test.
+
+    Args:
+      model: model under test
+      max_num_seqs: max sequences to schedule
+      max_num_batch_tokens: max num tokens to batch
+      enable_prefix_caching: optionally force APC config
+                             (True/False) or use default
+                             (None)
+
+    Returns:
+      {class}`Scheduler` instance
+    '''
+    if max_model_len is None:
+        max_model_len = max_num_batched_tokens
+    scheduler_config = SchedulerConfig(
+        max_num_seqs=max_num_seqs,
+        max_num_batched_tokens=max_num_batched_tokens,
+        max_model_len=max_model_len,
+        long_prefill_token_threshold=long_prefill_token_threshold,
+        disable_chunked_mm_input=disable_chunked_mm_input,
+        enable_chunked_prefill=True,
+    )
+    model_config = ModelConfig(
+        model=model,
+        task="auto",
+        tokenizer=model,
+        tokenizer_mode="auto",
+        trust_remote_code=True,
+        dtype="float16",
+        seed=42,
+    )
+    # Cache config, optionally force APC
+    kwargs_cache = ({} if enable_prefix_caching is None else {
+        'enable_prefix_caching': enable_prefix_caching
+    })
+    cache_config = CacheConfig(
+        block_size=block_size,
+        gpu_memory_utilization=0.9,
+        swap_space=0,
+        cache_dtype="auto",
+        **kwargs_cache,
+    )
+    kv_transfer_config = KVTransferConfig(
+        kv_connector="SharedStorageConnector",
+        kv_role="kv_both",
+        kv_connector_extra_config={"shared_storage_path": "local_storage"},
+    ) if use_kv_connector else None
+
+    speculative_config: Optional[SpeculativeConfig] = None
+    if num_speculative_tokens is not None:
+        speculative_config = SpeculativeConfig(
+            model="ngram", num_speculative_tokens=num_speculative_tokens)
+
+    vllm_config = VllmConfig(
+        scheduler_config=scheduler_config,
+        model_config=model_config,
+        cache_config=cache_config,
+        kv_transfer_config=kv_transfer_config,
+        speculative_config=speculative_config,
+    )
+    kv_cache_config = KVCacheConfig(
+        num_blocks=num_blocks,  # A large number of blocks to hold all requests
+        tensors={},
+        kv_cache_groups=[
+            KVCacheGroupSpec(['layer'],
+                             FullAttentionSpec(block_size, 1, 1, torch.float32,
+                                               False))
+        ],
+    )
+    cache_config.num_gpu_blocks = num_blocks
+    return Scheduler(
+        vllm_config=vllm_config,
+        kv_cache_config=kv_cache_config,
+        log_stats=True,
+        structured_output_manager=StructuredOutputManager(vllm_config),
+    )
+
+
+def create_requests(num_requests: int,
+                    num_tokens: int = 10,
+                    mm_positions: Optional[list[PlaceholderRange]] = None,
+                    max_tokens: int = 16,
+                    stop_token_ids: Optional[list[int]] = None,
+                    prompt_logprobs: Optional[int] = None):
+    sampling_params = SamplingParams(ignore_eos=False,
+                                     max_tokens=max_tokens,
+                                     stop_token_ids=stop_token_ids,
+                                     prompt_logprobs=prompt_logprobs)
+    requests = []
+    for i in range(num_requests):
+        if mm_positions is not None:
+            mm_position = mm_positions[i]
+            mm_inputs = [MultiModalKwargs({})] * len(mm_position)
+        else:
+            mm_position = None
+            mm_inputs = None
+        request = Request(
+            request_id=f"{i}",
+            prompt_token_ids=[i] * num_tokens,
+            sampling_params=sampling_params,
+            multi_modal_inputs=mm_inputs,
+            multi_modal_placeholders=mm_position,
+            multi_modal_hashes=None,
+            eos_token_id=EOS_TOKEN_ID,
+            arrival_time=0,
+        )
+        requests.append(request)
+    return requests
 
 def test_no_chunk():
     """Prefills should not be chunked without `FORCE_ENABLE_CHUNK_PREFILL` set."""
-    
-    from infer_engines.vllm.tests.v1.core.test_scheduler import create_scheduler, create_requests
 
     block_size = 4
     max_seqs = 60
@@ -33,17 +170,17 @@ def test_no_chunk():
     assert len(scheduler.running) == 1
     assert len(output.scheduled_new_reqs) == 1
     assert len(output.scheduled_cached_reqs) == 0
-    assert output.num_scheduled_tokens[requests[1].request_id] == 60
+    assert output.num_scheduled_tokens[requests[0].request_id] == 60
     assert output.total_num_scheduled_tokens == 60
-    requests[1].append_output_token_ids(1)
+    requests[0].append_output_token_ids(1)
 
     output = scheduler.schedule()
     assert len(scheduler.waiting) == 0
     assert len(scheduler.running) == len(requests)
     assert len(output.scheduled_new_reqs) == 1
     assert len(output.scheduled_cached_reqs) == 1
-    assert output.num_scheduled_tokens[requests[0].request_id] == 60
-    assert output.num_scheduled_tokens[requests[1].request_id] == 1
+    assert output.num_scheduled_tokens[requests[1].request_id] == 60
+    assert output.num_scheduled_tokens[requests[0].request_id] == 1
     assert output.total_num_scheduled_tokens == 61
 
 def test_chunk():
@@ -63,14 +200,11 @@ def test_chunk():
     model_path=f"{CUR_DIR}/mock_model/"
 
     with patch.dict(os.environ, env_vars):
-        # Clear module cache 
-        if 'infer_engines.vllm.tests.v1.core.test_scheduler' in sys.modules:
-            del sys.modules['infer_engines.vllm.tests.v1.core.test_scheduler']
-        if 'vllm.v1.core.sched.scheduler' in sys.modules:
-            del sys.modules['vllm.v1.core.sched.scheduler']
-
         # Reload module with new env var
-        from infer_engines.vllm.tests.v1.core.test_scheduler import create_scheduler, create_requests
+        from importlib import reload
+        import vllm.v1.core.sched.scheduler
+
+        reload(vllm.v1.core.sched.scheduler)
 
         scheduler = create_scheduler(model=model_path,
                                     max_num_seqs=max_seqs,
@@ -88,21 +222,19 @@ def test_chunk():
         assert len(scheduler.running) == len(requests)
         assert len(output.scheduled_new_reqs) == len(requests)
         assert len(output.scheduled_cached_reqs) == 0
-        assert output.num_scheduled_tokens[requests[0].request_id] == 4
-        assert output.num_scheduled_tokens[requests[1].request_id] == 60
+        assert output.num_scheduled_tokens[requests[0].request_id] == 60
+        assert output.num_scheduled_tokens[requests[1].request_id] == 4
         assert output.total_num_scheduled_tokens == 64
-        requests[1].append_output_token_ids(1)
+        requests[0].append_output_token_ids(1)
 
         output = scheduler.schedule()
         assert len(scheduler.waiting) == 0
         assert len(scheduler.running) == len(requests)
         assert len(output.scheduled_new_reqs) == 0
         assert len(output.scheduled_cached_reqs) == len(requests)
-        assert output.num_scheduled_tokens[requests[0].request_id] == 56
-        assert output.num_scheduled_tokens[requests[1].request_id] == 1
+        assert output.num_scheduled_tokens[requests[0].request_id] == 1
+        assert output.num_scheduled_tokens[requests[1].request_id] == 56
         assert output.total_num_scheduled_tokens == 57
-
-    if 'infer_engines.vllm.tests.v1.core.test_scheduler' in sys.modules:
-        del sys.modules['infer_engines.vllm.tests.v1.core.test_scheduler']
+        
     if 'vllm.v1.core.sched.scheduler' in sys.modules:
         del sys.modules['vllm.v1.core.sched.scheduler']
